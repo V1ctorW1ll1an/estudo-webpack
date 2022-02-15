8231c3bfc8c2fe398f743814ea0a736e0e2ad749
Victor Willian de Sousa <victorwillian1012@gmail.com>
2022-02-14 22:46:40 -0300

    configurando pasta de saida

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,8 @@ const webpack = require("webpack");
 module.exports = {
     mode: "development",
     entry: "./src/main.js",
+    output: {
+        filename: "main.js",
+        path: __dirname + "/public",
+    },
 };
