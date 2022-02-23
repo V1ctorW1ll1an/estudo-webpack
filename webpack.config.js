07189207f2c9d91b1e8d62dc870eded7b9a03554
Victor Willian de Sousa <victorwillian1012@gmail.com>
2022-02-22 22:59:13 -0300

    Adicionando webserver

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
         filename: "main.js",
         path: __dirname + "/public",
     },
+    devServer: {
+        port: 9000,
+    },
     optimization: {
         minimizer: [
             new UglifyJsPlugin({
